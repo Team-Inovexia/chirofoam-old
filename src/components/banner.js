3e00d638f89921d66f03ec7c8e5948d57ae78deb
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-18 14:26:13 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Header from '../components/header'
-import {Row, Col} from 'reactstrap';
 import "../assets/css/animate.css"
 import HomeBannerSlider from '../components/HomeBannerSlider'
 
 const Banner = () => {
-
-	return(
-	<header className="header-outer home-page">
-		<div className="banner-slider position-relative">
-			<HomeBannerSlider />
-		</div>
-		<Header color="dark" />
-	</header>
-	)
+  return (<header className="header-outer home-page">
+    <div className="banner-slider position-relative">
+      <HomeBannerSlider/>
+    </div>
+    <Header color="dark"/>
+  </header>)
 }
-
-export default Banner;
+export default Banner
