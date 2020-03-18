66f985bd9c044f8034b474a56771a0a9422be5d1
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-18 11:34:10 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,5 +1,6 @@
 (function() {
   function asyncLoad() {
+    var x = document.getElementsByTagName('script')[0];
     var urls = ["//www.google.com/recaptcha/api.js?render=explicit"];
     for (var i = 0; i < urls.length; i++) {
       var s = document.createElement('script');
