003c4ded3de7870b2026c8b79651c3c01d814e78
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-18 08:56:27 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -16,15 +16,12 @@ const beautified = [
   "./src/assets/css/font-awesome.css",
   "./src/fonts//fonts.css"
 ]
-minified.forEach((cssFile, i) => {
-  if (fs.existsSync(cssFile)) {
-    import cssFile;
-  } else {
-    import beautified[i];
-  }
-})
 // You can delete this file if you're not using it
-import "./src/assets/css/custom.css"
+if (fs.existsSync("./src/assets/css/custom.min.css")) {
+  import "./src/assets/css/custom.min.css";
+} else {
+  import "./src/assets/css/custom.css"
+}
 import "./src/assets/css/responsive.css"
 import "./src/assets/css/font-awesome.min.css"
 import "./src/fonts//fonts.css"
