f8100709a5e94240c3cf2e1cb4bca9b62b5d3cb3
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-18 09:02:32 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,25 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-const fs = require("fs")
-const minified = [
-  "./src/assets/css/custom.min.css",
-  "./src/assets/css/responsive.min.css",
-  "./src/assets/css/font-awesome.min.css",
-  "./src/fonts//fonts.min.css"
-]
-const beautified = [
-  "./src/assets/css/custom.css",
-  "./src/assets/css/responsive.css",
-  "./src/assets/css/font-awesome.css",
-  "./src/fonts//fonts.css"
-]
+
 // You can delete this file if you're not using it
-if (fs.existsSync("./src/assets/css/custom.min.css")) {
-  import "./src/assets/css/custom.min.css";
-} else {
-  import "./src/assets/css/custom.css"
-}
-import "./src/assets/css/responsive.css"
+import "./src/assets/css/custom.min.css"
+import "./src/assets/css/responsive.min.css"
 import "./src/assets/css/font-awesome.min.css"
-import "./src/fonts//fonts.css"
+import "./src/fonts/fonts.css"
