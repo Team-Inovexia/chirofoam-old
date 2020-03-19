738ea9babd4548ea3aa1aebd31e69605b1d07d3d
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-19 13:28:52 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/banner-shop-chirofoam.js
+++ src/components/banner-shop-chirofoam.js
@@ -11,7 +11,7 @@ const Banner = () => (
 					<span className="erbaum-bold color-primary space-1">SHOP</span>
 					<h2 className="shop-banner-title erbaum-bold color-primary">CHIROFOAM</h2>
 					<p className="cta mt-0 pt-sm-5 pt-lg-5 pt-xl-5">
-            <Link activeClass="active" className="btn-cta color-primary erbaum-bold space-1" to="product" spy={true} smooth={true} duration={250}>BUY NOW</Link>
+            <Link activeClass="active" className="btn-cta color-primary erbaum-bold space-1" to="product" containerId="product" spy={true} hashSpy={true} smooth={true} duration={250}>BUY NOW</Link>
 					</p>
 				</Col>
 				<Col md="5">
