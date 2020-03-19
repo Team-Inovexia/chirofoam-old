25749c1e25fccd6d925d4b50b08191b58c7189ae
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-19 14:20:10 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/spybutton.js
+++ src/components/spybutton.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Scroll from 'react-scroll'
 let ScrollLink = Scroll.ScrollLink;
 
-const Spybutton = props => {
+const spyButton = props => {
   return (
-    <button {...this.props}>
-    {this.props.children}
+    <button {...props}>
+    {props.children}
     </button>
   )
 }
-export default ScrollLink(Spybutton)
+export default ScrollLink(spyButton)
