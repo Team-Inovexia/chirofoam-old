be4f01f5de371b800bb81f5e56541203e60cf7d5
rajkumar-inovexia <rajkumar@innovagesoftwares.com>
2020-03-24 15:17:27 +0530

    Signed-off-by: rajkumar-inovexia <rajkumar@innovagesoftwares.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,10 +27,11 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `Chirofoam`,
-        start_url: `/`,
+        start_url: `/src/pages/index.js`,
         background_color: `#243b6b`,
         theme_color: `#243b6b`,
         display: `minimal-ui`,
+        scope: `/src/pages/index.js`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
